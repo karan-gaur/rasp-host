f32d445886ab391c614dcc7d5e342172a9705a44
Karan Gaur <karan.gaur@automationanywhere.com>
2020-01-28 18:31:46 +0530

    working on streaming

--- Node/routes/host.js
+++ Node/routes/host.js
@@ -12,40 +12,42 @@ const getSize = require('get-folder-size');
 // Root Api.
 // router.post('/', checkAuthentication, (req, res) => {
 router.post('/', (req, res) => {
-    selectedFile = req.body.filePath;
-    if( !fs.existsSync(selectedFile) ) {
+    if( !fs.existsSync(req.body.filePath) ) {
         res.sendStatus(403);
-    } else if( fs.lstatSync(selectedFile).isDirectory() ) {
-        res.json({ files: fs.readdirSync(selectedFile), selectedFile: selectedFile });
+    } else if( fs.lstatSync(req.body.filePath).isDirectory() ) {
+        res.json({ files: fs.readdirSync(req.body.filePath), selectedFile: req.body.filePath });
     } else {
-        const path = 'assets/sample.mp4'
-        const stat = fs.statSync(req.body.filePath)
-        const fileSize = stat.size
-        const range = req.headers.range
+        const path = req.body.filepath;
+        const stat = fs.statSync(req.body.filePath);
+        const fileSize = stat.size;
+        const range = req.headers.range;
+        console.log("1", range)
         
         if (range) {
-            const parts = range.replace(/bytes=/, "").split("-")
-            const start = parseInt(parts[0], 10)
-            const end = parts[1] ? parseInt(parts[1], 10) : fileSize-1
+            console.log("If Range")
+            const parts = range.replace(/bytes=/, "").split("-");
+            const start = parseInt(parts[0], 10);
+            const end = parts[1] ? parseInt(parts[1], 10) : fileSize-1;
             
-            const chunksize = (end-start) + 1
-            const file = fs.createReadStream(path, {start, end})
+            const chunksize = (end-start) + 1;
+            const file = fs.createReadStream(path, {start, end});
             const head = {
                 'Content-Range': `bytes ${start}-${end}/${fileSize}`,
                 'Accept-Ranges': 'bytes',
                 'Content-Length': chunksize,
                 'Content-Type': 'video/mp4',
-            }
+            };
             
-            res.writeHead(206, head)
-            file.pipe(res)
+            res.writeHead(206, head);
+            file.pipe(res);
         } else {
+            console.log("Else Range");
             const head = {
                 'Content-Length': fileSize,
                 'Content-Type': 'video/mp4',
-            }
-            res.writeHead(200, head)
-            fs.createReadStream(path).pipe(res)
+            };
+            res.writeHead(200, head);
+            fs.createReadStream(path).pipe(res);
         }
     }
 });
