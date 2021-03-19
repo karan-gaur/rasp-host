8089cc65fe2d9e12eb3b9649a799bd96576545fa
Karan Gaur <karan.gopal.gaur@gmail.com>
2021-03-19 17:39:39 +0530

    Improving model schema

--- Node/models/user.js
+++ Node/models/user.js
@@ -1,44 +1,36 @@
-const mongoose = require('mongoose');
-const config = require('../config')
+const mongoose = require("mongoose");
+const config = require("../config");
 
 var userSchema = new mongoose.Schema({
-    email : {
-        type : String,
-        unique : true,
-        required : true
-    },
-    name : {
-        type : String,
-        required : true,
-    },
-    hash : {
+    email: {
         type: String,
-        required: true
+        unique: true,
+        required: true,
     },
-    path : {
-        type : Array,
-        required : true
+    name: {
+        type: String,
+        required: true,
     },
-    admin : {
-        type : Boolean,
-        Default : false
+    hash: {
+        type: String,
+        required: true,
     },
-    storage : {
-        type : Number,
-        Default : 0
+    path: {
+        type: Array,
+        required: true,
     },
-    storageLimit : {
-        type : Number,
-        Default : config.USER_STORAGE_LIMIT
-    }
+    admin: {
+        type: Boolean,
+        Default: false,
+    },
+    storage: {
+        type: Number,
+        Default: 0,
+    },
+    storageLimit: {
+        type: Number,
+        Default: config.USER_STORAGE_LIMIT,
+    },
 });
 
-userSchema.methods.setHash = function(hash) {
-    this.hash = hash;
-};
-
-userSchema.methods.getHash = function(password) {
-    return this.hash;
-};
-
-module.exports = mongoose.model('User',userSchema);
+module.exports = mongoose.model("User", userSchema);
