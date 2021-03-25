0e6700dc58bd81afca1a344f93d342888761b0d4
Karan Gaur <karan.gopal.gaur@gmail.com>
2021-03-25 23:54:10 +0530

    Adding validator to check filepath

--- Node/utility.js
+++ Node/utility.js
@@ -42,6 +42,21 @@ function checkAuthentication(req, res, next) {
 }
 
 /**
+ * Check if given file exists for user.
+ * @param {ReqBody} req User API request object
+ * @param {ResBody} res User API response object
+ * @param {*} next Callback
+ */
+function checkFilePath(req, res, next) {
+    if (typeof req.body.filePath !== "string" || !fs.existsSync(req.body.filePath)) {
+        // Directory does not exists
+        logger.error(`No such file/folder exists - ${req.body.filePath}`);
+        return res.status(400).json({ error: "No such file/folder exists - '" + req.body.filePath + "'" });
+    }
+    next();
+}
+
+/**
  * User API authorization check.
  * @param {ReqBody} req User API request object
  * @param {ResBody} res User API response object
@@ -68,39 +83,40 @@ function checkAuthorization(req, res, next) {
  * @param {ResBody} res User API response object
  * @param {*} next Callback
  */
-function verifyPassword(req, res, next) {
-    if (typeof req.body.password === "undefined") {
+const verifyPassword = async (req, res, next) => {
+    if (typeof req.body.password !== "string") {
         logger.error(`Missing parameter 'password' for verifyPassword with token email - '${req.body.token.email}'`);
-        return res.status(400).json({ error: "Missing paramter in request body - 'password'." });
+        return res
+            .status(400)
+            .json({ error: "Invalid/Missing paramter 'password' - '" + req.body.password + "'. Must be string" });
     }
 
     try {
         // Verifying user password
-        User.findOne({ email: req.body.token.email }).then((usr) => {
-            if (usr) {
-                // User Exists
-                bcrypt.compare(req.body.password, usr.hash).then((resolve) => {
-                    if (!resolve) {
-                        // Invalid Password
-                        logger.info(`Invalid Password for - '${req.body.email}'`);
-                        return res.status(401).json({ error: "Invalid username/password" });
-                    } else {
-                        // Login Successful
-                        logger.info(`User password verfied - '${req.body.email}'`);
-                        next();
-                    }
-                });
-            } else {
-                // No such user
-                logger.info(`No user with username - '${req.body.email}'`);
-                return res.status(401).json({ error: "Invalid username/password" });
-            }
-        });
+        const usr = await User.findOne({ email: req.body.token.email }).exec();
+        if (usr) {
+            // User Exists
+            await bcrypt.compare(req.body.password, usr.hash).then((resolve) => {
+                if (!resolve) {
+                    // Invalid Password
+                    logger.error(`Invalid Password for - '${req.body.email}'`);
+                    return res.status(401).json({ error: "Invalid username/password" });
+                } else {
+                    // Login Successful
+                    logger.info(`User password verfied - '${req.body.token.email}'`);
+                    next();
+                }
+            });
+        } else {
+            // No such user
+            logger.info(`No user with username - '${req.body.email}'`);
+            return res.status(401).json({ error: "Invalid username/password" });
+        }
     } catch (err) {
         logger.error(`Error verifying user password - '${req.body.token.email}' - Err - ${err}`);
         return res.status(500).json({ error: "Internal server error. Contact System Administrator" });
     }
-}
+};
 
 /**
  * Evaluate folder size in Bytes
@@ -132,6 +148,7 @@ function getFolderSize(dirPath) {
  * @returns {Boolean} True if email is vald
  */
 function validateEmail(email) {
+    if (email.length < 6 || email.length > 256) return false;
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
 }
@@ -152,5 +169,6 @@ module.exports = {
     verifyPassword: verifyPassword,
     getFolderSize: getFolderSize,
     validateEmail: validateEmail,
+    checkFilePath: checkFilePath,
     getFileExtension: getFileExtension,
 };
