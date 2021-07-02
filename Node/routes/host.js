130351f47e44181e789b1ad98ed4862a7e0cb85b
Karan Gaur <karan.gopal.gaur@gmail.com>
2021-07-02 14:09:04 +0530

    Added validation middlewares to host.js

--- Node/routes/host.js
+++ Node/routes/host.js
@@ -1,23 +1,22 @@
 const express = require("express");
+const nodemailer = require("nodemailer");
+
+const config = require("../config");
 const constants = require("../constants");
 const utility = require("../utilities/utility");
+const check = require("../utilities/validationUtility");
 
 const router = express.Router();
 const logger = constants.LOGGER;
+const transport = nodemailer.createTransport(config.mailer);
 
 // Update user storage limit
-router.post("/storageLimit", [utility.checkAuthentication, utility.checkAuthorization], (req, res) => {
-    // Validating Body Params
-    if (typeof req.body.email !== "string") {
-        logger.error(`Invalid body attribute 'email' for '/storageLimit'. User - '${req.body.email}'`);
-        return res.status(400).json({ error: "Invalid value for 'email' - " + req.body.email });
-    } else if (typeof req.body.storageLimit !== "number" || req.body.storageLimit == 0) {
-        logger.error(`Invalid body attribute 'storageLimit' for '/storageLimit' - '${req.body.storageLimit}'`);
-        return res.status(400).json({ error: "Invalid value for 'storageLimit' - " + req.body.storageLimit });
-    }
-
-    User.findOne({ email: req.body.email })
-        .then((usr) => {
+router.post(
+    "/storageLimit",
+    [utility.checkAuthentication, utility.checkAuthorization, check.verifyEmail, check.validateStorageLimit],
+    async (req, res) => {
+        try {
+            const usr = await User.findOne({ email: req.body.email }).exec();
             if (usr) {
                 // User exists - updating user storage limit
                 logger.info(`Updating (${usr.email}) storage limit ${usr.storageLimit}=>${req.body.storageLimit}`);
@@ -39,61 +38,40 @@ router.post("/storageLimit", [utility.checkAuthentication, utility.checkAuthoriz
                     error: "Invalid username/password. Please re-login",
                 });
             }
-        })
-        .catch((err) => {
+        } catch (err) {
             logger.error(`Error updating storageLimit for user - '${req.body.token.email}'. Error - ${err}`);
             return res.status(500).json({ error: "Internal server error. Contact System Administrator" });
-        });
-});
+        }
+    }
+);
 
 // Get user's list using pagination for lazy loading
-router.get("/getUsers", [utility.checkAuthentication, utility.checkAuthorization], (req, res) => {
-    const page = parseInt(req.query.page);
-    const limit = parseInt(req.query.limit);
+router.get(
+    "/getUsers",
+    [utility.checkAuthentication, utility.checkAuthorization, check.validatePaginationParams],
+    (req, res) => {
+        const page = parseInt(req.query.page);
+        const limit = parseInt(req.query.limit);
 
-    // Validating query params
-    if (isNaN(limit) || isNaN(page)) {
-        logger.error(
-            `Query values are not a Number for endpoint - '/getUsers' - {'page':${page}, ` +
-                `'limit':${limit}, 'email':'${req.body.token.email}'}`
-        );
-        return res.status(400).json({ error: "Invalid query params - Required - {'limit':Number, 'page':Number}" });
+        User.find({}, { _id: 0, __v: 0, path: 0, hash: 0 })
+            .skip((page - 1) * limit)
+            .limit(limit)
+            .sort({ name: "asc" })
+            .exec((err, users) => {
+                if (err) {
+                    logger.error(
+                        `Error fetching paginated values {'page':${page}, 'limit':${limit}, 'email':` +
+                            `'${req.body.token.email}'}. Error - ${err}`
+                    );
+                    return res.status(500).json({ error: "Internal server error. Contact System Administrator" });
+                }
+                return res.status(200).json({ users: users });
+            });
     }
-
-    User.find({}, { _id: 0, __v: 0, path: 0, hash: 0 })
-        .skip((page - 1) * limit)
-        .limit(limit)
-        .sort({ name: "asc" })
-        .exec((err, users) => {
-            if (err) {
-                logger.error(
-                    `Error fetching paginated values {'page':${page}, 'limit':${limit}, 'email':` +
-                        `'${req.body.token.email}'}. Error - ${err}`
-                );
-                return res.status(500).json({ error: "Internal server error. Contact System Administrator" });
-            }
-            return res.status(200).json({ users: users });
-        });
-});
+);
 
 // GET Contact Us page.
-router.post("/contact", (req, res) => {
-    // Validating body params
-    if (typeof req.body.name !== "string" || req.body.name.length == 0 || req.body.name.length > 32) {
-        logger.error(`Invalid '/register' body param - {'name':'${req.body.name}'}`);
-        return res
-            .status(400)
-            .json({ error: "Invalid value for 'name' - '" + req.body.name + "'. Must be string [1-32 Chars]." });
-    } else if (typeof req.body.email !== "string" || !utility.validateEmail(req.body.email)) {
-        logger.error(`Invalid '/register' body param - {'email':'${req.body.email}'}`);
-        return res.status(400).json({ error: "Invalid syntax for 'email' - '" + req.body.email + "'." });
-    } else if (typeof req.body.message !== "string" || req.body.message.length <= 0) {
-        logger.error(`Invalid '/message' body param - {'message':'${req.body.message}'}. Must be non-empty string`);
-        return res
-            .status(400)
-            .json({ error: "Invalid value for 'email' - '" + req.body.email + "'. Must be string [1-32 Chars]." });
-    }
-
+router.post("/contact", [check.validateName, check.validateEmail, check.validateContactMessage], (req, res) => {
     // Mailing client details
     const mailoptions = {
         from: req.body.email,
